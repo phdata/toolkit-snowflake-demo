2fae863a2ba0cef13d97279362e4c4b97e535a6a
Daniel Pierce <dpierce@phdata.io>
2024-10-14 10:02:15 -0500

    misc: Flatten and simplify directory structure
    Remove functionality directory
    
    This content is now in https://github.com/phdata/snowflake-feature-demo

--- functionality/hybrid-tables/queries/transaction.sql
+++
@@ -1,8 +0,0 @@
-SET USE_CACHED_RESULT = false;
-USE DATABASE hybrid_table_demo;
-
-BEGIN;
-DELETE FROM hybrid.order_history WHERE CUSTOMER_ID = 1;
-DELETE FROM hybrid.customer WHERE id = 1;
-COMMIT;
-SELECT * FROM hybrid.order_history WHERE CUSTOMER_ID = 1;
--- functionality/hybrid-tables/templates/queries/filter-single-record-pk.sql
+++
@@ -1,6 +0,0 @@
-SET USE_CACHED_RESULT = false;
-USE DATABASE hybrid_table_demo;
-
-alter session set query_tag = '{"type": "{{TYPE}}", "description": "Filter single record using INTEGER primary key", "application": "benchmark", "session": "{{SESSION_ID}}"}';
-
-SELECT * FROM {{TYPE}}.customer WHERE id = 1;
